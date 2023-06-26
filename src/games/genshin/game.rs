51308098a6f6251365494a4755b881becde7187a
Renaud Lepage <root@cybikbase.com>
2023-06-26 16:26:05 -0700

    99.1.8.4 rewrite
    * more rustisms
    * less hamfisted things
    * disable SDK on disabled-patch runs
    * no fps unlock on Steam & deck. Opinionated.

--- src/games/genshin/game.rs
+++ src/games/genshin/game.rs
@@ -63,7 +63,7 @@ pub fn run() -> anyhow::Result<()> {
     let features = wine.features(&config.components.path)?.unwrap_or_default();
 
     let mut folders = Folders {
-        wine: config.game.wine.builds.join(&wine.name),
+        wine: wine.get_runner_dir(config.game.wine.builds.clone()),
         prefix: config.game.wine.prefix.clone(),
         game: config.game.path.for_edition(config.launcher.edition).to_path_buf(),
         temp: config.launcher.temp.clone().unwrap_or(std::env::temp_dir())
@@ -74,7 +74,11 @@ pub fn run() -> anyhow::Result<()> {
     tracing::info!("Checking telemetry");
 
     if let Ok(Some(server)) = telemetry::is_disabled(config.launcher.edition) {
-        return Err(anyhow::anyhow!("Telemetry server is not disabled: {server}"));
+        if config.patch.apply {
+            return Err(anyhow::anyhow!("Telemetry server is not disabled: {server}"));
+        } else {
+            tracing::warn!("Telemetry server is not disabled ({server}) but patch is disabled.");
+        }
     }
 
     // Prepare fps unlocker
@@ -152,9 +156,13 @@ pub fn run() -> anyhow::Result<()> {
     }
 
     windows_command += if config.game.enhancements.fps_unlocker.enabled && cfg!(feature = "fps-unlocker") {
-        "fps_unlocker.bat "
+        "fps_unlocker.bat " // completely disabled in steam integration mode.
     } else {
-        "launcher.bat "
+        if !config.patch.apply {
+            "GenshinImpact.exe "
+        } else {
+            "launcher.bat "
+        }
     };
 
     if config.game.wine.borderless {
