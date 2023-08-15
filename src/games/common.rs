5cd73e8df618b6bc647a02cc59e42b3d77bee76e
Renaud Lepage <root@cybikbase.com>
2023-08-15 07:17:22 -0700

    nit

--- src/games/common.rs
+++ src/games/common.rs
@@ -11,10 +11,17 @@ pub fn base_install_dir(launcher_dir: PathBuf) -> anyhow::Result<PathBuf> {
             Ok(launcher_dir)
         },
         steam::LaunchedFrom::Steam => {
-            Ok(match steam::get_steam_compatdata_cdrive_root() {
-                Some(path) => PathBuf::from(path),
-                None => launcher_dir
-            })
+            match steam::aagl_launcher_launch_target() {
+                Some(target) => {
+                    Ok(PathBuf::from(target).parent().unwrap().to_path_buf())
+                },
+                None => {
+                    return Ok(match steam::get_steam_compatdata_cdrive_root() {
+                        Some(path) => PathBuf::from(path),
+                        None => launcher_dir
+                    })
+                }
+            }
         }
     }
 }
